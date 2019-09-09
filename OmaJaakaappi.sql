b019de18ef727d3fea8fca33a6ee5938336d334d
villekerri <36498749+villekerri@users.noreply.github.com>
2019-09-09 13:54:34 +0300

    Add files via upload

---
+++ OmaJaakaappi.sql
@@ -0,0 +1,46 @@
+CREATE TABLE tuote
+(
+  tuote_id INT NOT NULL,
+  tuote_nimi VARCHAR(100) NOT NULL,
+  tuote_yksikko VARCHAR(100),
+  tuote_kcal INT,
+  PRIMARY KEY (tuote_id)
+);
+
+CREATE TABLE jaakaappi
+(
+  jaakaappi_id INT NOT NULL,
+  tuote_pvm DATE,
+  tuote_maara INT,
+  tuote_status VARCHAR(100) NOT NULL,
+  tuote_id INT NOT NULL,
+  PRIMARY KEY (jaakaappi_id),
+  FOREIGN KEY (tuote_id) REFERENCES tuote(tuote_id)
+);
+
+CREATE TABLE resepti
+(
+  resepti_id INT NOT NULL,
+  resepti_ohje VARCHAR(1000) NOT NULL,
+  PRIMARY KEY (resepti_id)
+);
+
+CREATE TABLE rpk
+(
+  rpk_id INT NOT NULL,
+  rpk_pvm DATE NOT NULL,
+  jaakaappi_id INT NOT NULL,
+  PRIMARY KEY (rpk_id),
+  FOREIGN KEY (jaakaappi_id) REFERENCES jaakaappi(jaakaappi_id)
+);
+
+CREATE TABLE ainekset
+(
+  aines_id INT NOT NULL,
+  aines_maara INT,
+  tuote_id INT NOT NULL,
+  resepti_id INT NOT NULL,
+  PRIMARY KEY (aines_id),
+  FOREIGN KEY (tuote_id) REFERENCES tuote(tuote_id),
+  FOREIGN KEY (resepti_id) REFERENCES resepti(resepti_id)
+);
