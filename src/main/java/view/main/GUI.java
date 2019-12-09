a347462726af633bec670fa5b35bafd27ca46872
tuppe35 <tuukka.rissanen@metropolia.fi>
2019-12-09 15:32:31 +0200

    Welcome Screen

--- src/main/java/view/main/GUI.java
+++ src/main/java/view/main/GUI.java
@@ -1,26 +1,29 @@
 package view.main;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class GUI extends Application{
 	
 	@Override
 	public void start(final Stage stage) throws Exception {
-		
-		Language language = Language.getInstance();
-		ResourceBundle bundle = ResourceBundle.getBundle("TextResources", language.getLocale());
-		Parent root = FXMLLoader.load(getClass().getResource("NavBar.fxml"), bundle);
-		
+
+		Parent root = FXMLLoader.load(getClass().getResource("Splash.fxml"));
 		Scene scene = new Scene(root);
+		
+		stage.initStyle(StageStyle.UNDECORATED);
 		stage.setScene(scene);
 		stage.show();
 	}
+		
 
 	public static void main(String[] args) {
 		launch(args);
